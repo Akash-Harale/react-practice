d935447565344b1f231210194bea47c8c54e9d47
akashvharale <akash.v.harale@gmail.com>
2025-10-15 20:00:43 +0530

    pattern question

---
+++ DSA/pattern.js
@@ -0,0 +1,14 @@
+let n = 4;
+for (let i = 0; i < n; i++) {
+  let bag = "";
+  for (let j = 0; j < n; j++) {
+    bag += " *";
+  }
+
+  console.log(bag);
+}
+
+// * * * *
+// * * * *
+// * * * *
+// * * * *
