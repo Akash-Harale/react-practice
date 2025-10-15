29f847fc8053fd75b2c16c123f2693463ea0ccc3
akashvharale <akash.v.harale@gmail.com>
2025-10-15 20:27:52 +0530

    new pattern

--- DSA/pattern.js
+++ DSA/pattern.js
@@ -20,11 +20,29 @@
 // * * *
 // * * * *
 
-let n = 4;
-for (let i = 0; i < n; i++) {
+// let n = 4;
+// for (let i = 0; i < n; i++) {
+//   let bag = "";
+//   for (let j = 0; j <= i; j++) {
+//     bag += " *";
+//   }
+//   console.log(bag);
+// }
+
+// -------------------------------------
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
   let bag = "";
-  for (let j = 0; j <= i; j++) {
-    bag += " *";
+  for (let j = 1; j <= i; j++) {
+    bag += ` ${j}`;
   }
   console.log(bag);
 }
